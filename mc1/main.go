b611d4df571f1984c5f4a6c14266d095d097893b
William Alberto Ugalde Gamboa <wugalde@beenverified.com>
2020-01-19 19:31:46 -0600

    Secure API using tokens to authorize requests

--- mc1/main.go
+++ mc1/main.go
@@ -4,11 +4,17 @@ import (
 	"net/http"
 
 	"github.com/wugalde19/pratik/mc1/lib/endpoints"
+	"github.com/wugalde19/pratik/mc1/lib/services"
+	"github.com/wugalde19/pratik/mc1/middleware"
 	"goji.io"
 )
 
 func main() {
 	mux := goji.NewMux()
+
+	authenticator := services.NewAuthenticator()
+
+	mux.Use(middleware.AuthMiddleware(authenticator))
 
 	endpoints.RegisterAll(mux)
 
